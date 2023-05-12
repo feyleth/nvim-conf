5d17175bea6d527162ca724ae941beadf5c69dbc
feyleth <feyleth@gmail.com>
2023-05-12 14:36:17 +0200

    add better ui for vim and signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,13 +2,6 @@ return {
     {
         "neovim/nvim-lspconfig",
         event={ "BufReadPre", "BufNewFile" },
-        dependencies={"ray-x/lsp_signature.nvim",opts={
-            bind = true, -- This is mandatory, otherwise border config won't get registered.
-            hint_prefix = "",
-    handler_opts = {
-      border = "rounded"
-    }
-        },lazy=true},
         config=function ()
             local lspconfig = require('lspconfig')
             local lsp_defaults = lspconfig.util.default_config
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,34 @@
+return {
+    "folke/noice.nvim",
+    dependencies={
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify"
+    },
+    event = "VeryLazy",
+    opts={
+        lsp = {
+            progress={
+                enabled=false,
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            },
+        },
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+        },
+        routes = {
+            {
+                filter = {
+                    event = "msg_show",
+                    kind = "",
+                },
+                opts = { skip = true },
+            },
+        },
+
+    }
+}
