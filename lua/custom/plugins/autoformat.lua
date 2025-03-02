7a6cd5acd76ae19a21905678811da907cd8cc2be
feyleth <feyleth@gmail.com>
2025-03-02 12:27:35 +0100

    add new plugin

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -13,6 +13,11 @@ return { -- Autoformat
   },
   opts = {
     notify_on_error = false,
+    format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
