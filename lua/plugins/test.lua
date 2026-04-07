2faad2aa1b39114adc738c8614561e4f46321d21
feyleth <43912085+feyleth@users.noreply.github.com>
2026-04-07 11:06:11 +0200

    ajout de plugin

---
+++ lua/plugins/test.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "rcasia/neotest-java",
+    ft = "java",
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        "neotest-java",
+      },
+    },
+  },
+}
