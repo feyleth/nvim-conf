be37a3fbf284c0d391db7096f0f4b50967d87f1f
Flavien Perineau <flavien.perineau-ext@francetravail.fr>
2026-04-16 09:53:34 +0200

    add format on save

--- plugin/format.lua
+++ plugin/format.lua
@@ -1,18 +1,21 @@
-
 Config.later(function()
-  vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+  vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
 
   -- See also:
   -- - `:h Conform`
   -- - `:h conform-options`
   -- - `:h conform-formatters`
-  require("conform").setup({
+  require('conform').setup {
     default_format_opts = {
       -- Allow formatting from LSP server if no dedicated formatter is available
-      lsp_format = "fallback",
+      lsp_format = 'fallback',
+    },
+    format_on_save = {
+      timeout_ms = 1000,
+      lsp_format = 'fallback',
     },
     -- Map of filetype to formatters
     -- Make sure that necessary CLI tool is available
-    formatters_by_ft = { lua = { "stylua" } },
-  })
+    formatters_by_ft = { lua = { 'stylua' } },
+  }
 end)
