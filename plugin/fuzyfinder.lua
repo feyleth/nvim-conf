f92af4f3bd20c9f01d04ef4c2c2f76ddd792cd48
Flavien Perineau <flavien.perineau-ext@francetravail.fr>
2026-04-20 10:54:35 +0200

    change default selector

--- plugin/fuzyfinder.lua
+++ plugin/fuzyfinder.lua
@@ -1,14 +1,17 @@
-Config.later(function ()
-  vim.pack.add({
-    "https://github.com/nvim-telescope/telescope.nvim",
-    "https://github.com/nvim-lua/plenary.nvim",
-  })
-  require('telescope').setup{
-      defaults = {
-          path_display={"smart"}
-      }
+Config.later(function()
+  vim.pack.add {
+    'https://github.com/nvim-telescope/telescope.nvim',
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-telescope/telescope-ui-select.nvim',
   }
-  local builtin = require('telescope.builtin')
+  require('telescope').setup {
+    defaults = {
+      path_display = { 'smart' },
+    },
+  }
+  require('telescope').load_extension 'ui-select'
+
+  local builtin = require 'telescope.builtin'
 
   vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
   vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
@@ -19,5 +22,4 @@ Config.later(function ()
   vim.lsp.buf.implementation = builtin.lsp_implementations
   vim.lsp.buf.definition = builtin.lsp_definitions
   vim.lsp.buf.type_definition = builtin.lsp_type_definitions
-
 end)
