980af5c8243be4cb145ae7b2ac1f128fd04c2098
Flavien Perineau <flavien.perineau-ext@francetravail.fr>
2026-05-11 10:57:10 +0200

    change nvim tree open file

--- plugin/tree.lua
+++ plugin/tree.lua
@@ -1,23 +1,23 @@
-Config.later(function ()
-  vim.pack.add({"https://github.com/nvim-tree/nvim-tree.lua","https://github.com/folke/snacks.nvim"})
-  
-  require("nvim-tree").setup({
-    renderer={
-      group_empty=true
-    }
-  })
+Config.later(function()
+  vim.pack.add { 'https://github.com/nvim-tree/nvim-tree.lua', 'https://github.com/folke/snacks.nvim' }
 
-  vim.keymap.set("n","\\","<cmd>NvimTreeOpen<cr>",{silent=true})
+  require('nvim-tree').setup {
+    renderer = {
+      group_empty = true,
+    },
+  }
 
-  local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "NvimTreeSetup",
+  vim.keymap.set('n', '\\', '<cmd>NvimTreeFindFile<cr>', { silent = true })
+
+  local prev = { new_name = '', old_name = '' } -- Prevents duplicate events
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'NvimTreeSetup',
     callback = function()
-      local events = require("nvim-tree.api").events
+      local events = require('nvim-tree.api').events
       events.subscribe(events.Event.NodeRenamed, function(data)
         if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
           data = data
-          require("snacks").rename.on_rename_file(data.old_name, data.new_name)
+          require('snacks').rename.on_rename_file(data.old_name, data.new_name)
         end
       end)
     end,
