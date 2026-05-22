34dcde46132a81b9fb6c1114c987597310a72b17
Test User <test@example.com>
2026-05-22 11:49:47 +0200

    change keymap

--- plugin/20_keymap.lua
+++ plugin/20_keymap.lua
@@ -11,10 +11,13 @@
 -- An example helper to create a Normal mode mapping
 local nmap = function(lhs, rhs, desc)
   -- See `:h vim.keymap.set()`
-  vim.keymap.set('n', lhs, rhs, { desc = desc ,silent=true})
+  vim.keymap.set('n', lhs, rhs, { desc = desc, silent = true })
 end
 
-nmap("<Esc><Esc>", "<cmd>nohl<cr>","disable higlight")
-vim.keymap.set('v', ">", ">gv", {silent=true})
-vim.keymap.set('v', "<", "<gv", {silent=true})
-nmap("<leader>e",vim.diagnostic.open_float,"Line Diagnostic")
+vim.keymap.set({ 'n', 's', 'i' }, '<esc>', function()
+  vim.cmd 'noh'
+  return '<esc>'
+end, { desc = 'Escape and Clear hlsearch', expr = true })
+vim.keymap.set('v', '>', '>gv', { silent = true })
+vim.keymap.set('v', '<', '<gv', { silent = true })
+nmap('<leader>e', vim.diagnostic.open_float, 'Line Diagnostic')
